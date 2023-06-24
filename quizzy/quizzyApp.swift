a0acf61e66bc9109413b43292b601312b168191b
Celine-ST <celine.quek10@rafflesgirlssch.edu.sg>
2023-06-24 10:12:09 +0800

    Initial Commit

---
+++ quizzy/quizzyApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizzyApp.swift
+//  quizzy
+//
+//  Created by Celine Quek on 24/6/23.
+//
+
+import SwiftUI
+
+@main
+struct quizzyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
